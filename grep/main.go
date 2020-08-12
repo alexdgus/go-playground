f81f7cc39476ce345746d3f0349098ebc2e53154
Alex <alexdgus@gmail.com>
2020-08-12 10:38:26 -0500

    Add ability to search multiple directories

--- grep/main.go
+++ grep/main.go
@@ -8,23 +8,41 @@ import (
 )
 
 func main() {
-	dir := "./"
-	if len(os.Args) > 1 {
-		dir = os.Args[1]
-	}
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, file := range files {
-		if file.IsDir() {
-			fmt.Println("file", file.Name())
+	var recursive bool
+	var dirs []string
+	for i, arg := range os.Args {
+		if i == 0 {
+			continue
+		}
+		if arg == "-r" {
+			recursive = true
+			fmt.Println(arg)
+		} else {
+			dirs = append(dirs, arg)
+			fmt.Println(arg)
 		}
 	}
-	for _, file := range files {
-		if !file.IsDir() {
-			fmt.Println("directory", file.Name())
+
+	dirs = append(dirs, "./")
+
+	if recursive {
+		for _, dir := range dirs {
+			fmt.Println(dir)
+			files, err := ioutil.ReadDir(dir)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for _, file := range files {
+				if file.IsDir() {
+					fmt.Println("file", file.Name())
+				}
+			}
+			for _, file := range files {
+				if !file.IsDir() {
+					fmt.Println("directory", file.Name())
+				}
+			}
 		}
 	}
 }
