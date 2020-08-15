125e07c1cddefcdcf7af6cc849ff91ad1a5af7f5
Alex <alexdgus@gmail.com>
2020-08-15 12:47:13 -0500

    Improve input handling

--- grep/main.go
+++ grep/main.go
@@ -9,42 +9,67 @@ import (
 	"strings"
 )
 
-func main() {
-	var recursive bool
-	var dirs []string
+func parseInput() (recursive bool, searchString string, dirs []string) {
+	r := false
+	var ss string
+	dirList := []string{}
 	for i, arg := range os.Args {
 		if i == 0 {
 			continue
-		}
-		if arg == "-r" {
-			recursive = true
+		} else if i == 1 && arg == "-r" {
+			r = true
 			fmt.Println(arg)
+		} else if i == 1 {
+			ss = arg
+		} else if i == 2 && recursive {
+			ss = arg
 		} else {
-			dirs = append(dirs, arg)
+			dirList = append(dirs, arg)
 			fmt.Println(arg)
 		}
 	}
+	if len(dirList) == 0 {
+		dirList = append(dirList, ".")
+	}
+	return r, ss, dirList
+}
 
-	dirs = append(dirs, "./")
+func main() {
+	recursive, searchString, dirs := parseInput()
 
-	if recursive {
+	for _, input := range os.Args {
+		fmt.Println(input)
+	}
+	fmt.Println("dirs found")
+	fmt.Println(dirs)
+
+	var files []string
+	for _, dir := range dirs {
+		files = findFiles(dir, recursive)
+	}
+	fmt.Println("files found")
+	fmt.Println(files)
+	for _, file := range files {
+		parse(file, searchString)
 	}
 }
 
-func findFiles(dirPath string, recursive bool) {
-	fmt.Println(dirPath)
+func findFiles(dirPath string, recursive bool) []string {
+	var filePaths []string
 	files, err := ioutil.ReadDir(dirPath)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		return filePaths
 	}
 
 	for _, file := range files {
 		if file.IsDir() && recursive {
-			findFiles(file.Name(), recursive)
-		} else {
-			parse(file.Name(), "for")
+			filePaths = append(filePaths, findFiles(file.Name(), recursive)...)
+		} else if !file.IsDir() {
+			filePaths = append(filePaths, dirPath+"/"+file.Name())
 		}
 	}
+	return filePaths
 }
 
 func parse(path string, searchStr string) {
@@ -59,12 +84,12 @@ func parse(path string, searchStr string) {
 	line := 1
 	for scanner.Scan() {
 		if strings.Contains(scanner.Text(), searchStr) {
-			fmt.Println("String found:", line)
+			fmt.Println("Line:", line, scanner.Text())
 		}
 		line++
 	}
 
 	if err := scanner.Err(); err != nil {
-		// Handle the error
+		log.Fatal(err)
 	}
 }
