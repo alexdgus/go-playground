745a76d9d073663d5938cdacfa65c4807b9c07f6
Alex <alexdgus@gmail.com>
2020-08-28 07:57:14 -0500

    Add more template stuff

--- http-server/main.go
+++ http-server/main.go
@@ -13,11 +13,25 @@ func (h hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	req.ParseForm()
 	fmt.Println(req.Form)
 
+	type Name struct {
+		FirstName string
+		LastName  string
+	}
+	var name Name
+
+	fmt.Println(req.Form)
+	if len(req.Form["fn"]) > 0 {
+		name.FirstName = req.Form["fn"][0]
+	}
+	if len(req.Form["ln"]) > 0 {
+		name.LastName = req.Form["ln"][0]
+	}
+
 	t, err := template.ParseFiles("index.gohtml")
 	if err != nil {
 		log.Println(err)
 	}
-	err = t.ExecuteTemplate(w, "index.gohtml", req)
+	err = t.ExecuteTemplate(w, "index.gohtml", name)
 }
 
 func main() {
